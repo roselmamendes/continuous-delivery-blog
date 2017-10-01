fce1bdd30d31b0fdd21552dc9074b7365d9322f5
Roselma Mendes <roselma.mendes@gmail.com>
2017-10-01 13:36:02 -0300

    Read a port configured by heroku.

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(5000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Blog listening on port 5000!');
 })
